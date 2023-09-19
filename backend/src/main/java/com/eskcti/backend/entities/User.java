e432bba0d1061fb8a39346faa3857cfad8673671
eskokado <eskokado@gmail.com>
2023-09-19 11:43:16 -0300

    feat: create entity User

---
+++ backend/src/main/java/com/eskcti/backend/entities/User.java
@@ -0,0 +1,22 @@
+package com.eskcti.backend.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tab_restaurants")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}

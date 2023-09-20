bb1b25317d39e6e126d2404fb6f193690f115706
eskokado <eskokado@gmail.com>
2023-09-20 13:52:20 -0300

    feat: add route user create

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { UserCrudComponent } from './views/user-crud/user-crud.component';
+import { UserCreateComponent } from './components/user/user-create/user-create.component'
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'users',
     component: UserCrudComponent
+  },
+  {
+    path: 'users/create',
+    component: UserCreateComponent
   }
 ];
 

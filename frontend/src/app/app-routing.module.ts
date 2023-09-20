ada9692e194f95cb71941f9a30dc677abbc9ce34
eskokado <eskokado@gmail.com>
2023-09-20 12:11:13 -0300

    feat: create nav component and impl

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { UserCrudComponent } from './views/user-crud/user-crud.component';
+
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'users',
+    component: UserCrudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
